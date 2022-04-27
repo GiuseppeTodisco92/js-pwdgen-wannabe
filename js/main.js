520c5ea32d5f67269445eb0bc2ae8f5da952bb80
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-04-27 13:03:37 +0200

    add programma

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//1.Chiedi all’utente il suo nome
+const name = prompt('inserisci il tuo nome');
+console.log(name);
+//2.poi chiedi il suo cognome
+const surname = prompt('inserisci il tuo cognome');
+console.log(surname);
+//3.poi chiedi il suo colore preferito
+const color = prompt('inserisci il tuo colore');
+console.log(color);
+//4.Infine scrivi sulla pagina nomecognomecolorepreferito21
+alert(name + surname + color + '21');
+document.writeln(name, surname, color, '21');
